f24d1ef73acba1714446d90555dd5fd1cd646e68
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-05-23 23:41:54 -0500

    task3 Write a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """class rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, new_width):
+        if type(new_width) is not int:
+            raise TypeError("width must be an integer")
+        if new_width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = new_width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, new_height):
+        if type(new_height) is not int:
+            raise TypeError("height must be an integer")
+        if new_height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = new_height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """Prints square to stdout"""
+        result = ""
+        if self.__width == 0 or self.__height == 0:
+            return result
+        else:
+            result += '\n'.join("#" * self.__width
+                                for j in range(self.__height))
+        return result
