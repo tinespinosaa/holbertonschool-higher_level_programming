a085acb1cf516c14292867d0f842c40f7cf33a4f
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-06-04 17:30:22 -0500

    task6 Write a class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Implementing a Geometry class
+"""
+
+
+class BaseGeometry:
+    """A class BaseGeometry with public method area"""
+    def area(self):
+        """
+            Calculates the area or raises an exception when called
+        """
+        raise Exception("area() is not implemented")
