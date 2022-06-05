69cb922cb3689c58cecd6fdb1f383d2e33862120
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-06-04 20:44:30 -0500

    task7 Write a script that adds all arguments to a Python list, and then save them to a file..

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-""" Function that creates an Object from a JSON file """
-import json
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""Module 7-add_item.
+Adds all arguments to a Python list,
+and then save them to a file.
+"""
 
-try:
-    new_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    save_to_json_file([], 'add_item.json')
-finally:
-    lis = []
-    for i in range(1, len(sys.argv)):
-        lis.append(sys.argv[i])
-    new_list = load_from_json_file('add_item.json')
-    new_list += lis
-    save_to_json_file(new_list, 'add_item.json')
+import sys
+import json
+import os.path
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+my_file = 'add_item.json'
+
+my_list = []
+
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
