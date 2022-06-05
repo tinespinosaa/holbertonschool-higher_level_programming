9b8fcbdcf021e4e322c7f0c12beb7d94d950769a
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-06-04 20:53:03 -0500

    task10 Write a class Student that defines a student by: (based on 9-student.py)..

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""10. Student to JSON with filter"""
+"""
+Student to JSON
+"""
 
 
 class Student:
-    """A class Student"""
-
+    """
+    Class creation to return the representation of the dictionary
+    """
     def __init__(self, first_name, last_name, age):
-        """Initializes an object for a Student class
-        Args:
-            first_name: a string
-            last_name: a string
-            age: a string
-        """
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns the dictionary description for JSON serialization of an object
-        Args:
-            obj: An abject
-        Returns:
-            A dictionary
-        """
-        if attrs is None or type(attrs) is not list:
-            return self.__dict__
-        if not (isinstance(attrs[i], str) for i in range(len(attrs))):
-            return self.__dict__
-        return {key: value for (key, value) in self.__dict__.items()
-                if key in attrs}
+        if(type(attrs) is list):
+            new_list = {}
+            for i in attrs:
+                try:
+                    new_list[i] = self.__dict__[i]
+                except Exception:
+                    pass
+        else:
+            new_list = self.__dict__
+
+        return new_list
