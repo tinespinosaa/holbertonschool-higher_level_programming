6149ccbe281ea768e549ece9931c0bcb1056d466
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-06-04 20:10:53 -0500

    task7 Write a script that adds all arguments to a Python list, and then save them to a file...

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""
-A script that adds all arguments to a Python list,\
- and then saves them to a file
-"""
+"""7. A script that adds all arguments to a Python list, and then save
+them to a file"""
+
 
 import sys
-save_to_json = __import__("5-save_to_json_file").save_to_json_file
-load_from_json = __import__("6-load_from_json_file").load_from_json_file
+import os.path
 
+save_to = __import__('5-save_to_json_file').save_to_json_file
+load_from = __import__('6-load_from_json_file').load_from_json_file
 
-def add_item(args, filename):
-    """
-        adds items to a json file
-        @args: The arguments that need to be added.
-        @filename: The file that needs to be updated
-    """
-    try:
-        content = load_from_json(filename)
-    except:
-        content = []
+filename = 'add_item.json'
 
-    for item in args:
-        content.append(item)
-    save_to_json(content, filename)
+if os.path.isfile(filename):
+    lst = load_from(filename)
+else:
+    lst = []
 
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        lst.append(sys.argv[i])
 
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    filename = "add_item.json"
-    add_item(args, filename)
+save_to(lst, filename)
