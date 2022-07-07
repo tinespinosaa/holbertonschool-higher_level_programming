5de42f460abbb54ecef799694e85664e9004ee57
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-07-06 22:42:06 -0500

    task10 Import the database dump from hbtn_0d_tvshows to your MySQL server: download.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,7 @@
--- 10. list all shows in db that have at least one genre
--- download thingy and import to mysql
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
