ad1cd1afd86b6756195e70b1fa300b80f99b654b
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-08-07 23:58:38 -0500

    task8 Write a script that prints the first State object from the database hbtn_0e_6_usa..

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,24 @@
-
-
 #!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+"""Prints the first State object"""
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
 
 if __name__ == "__main__":
-    import sqlalchemy
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
-    import sys
-
-    sub_lochost = 'mysql+mysqldb://{}:{}@localhost: 3306/{}'
-    engine = sqlalchemy.create_engine(sub_lochost.format
-                                      (sys.argv[1], sys.argv[2],
-                                       sys.argv[3]))
-
+    sql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(sql.format(argv[1],
+                                      argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
     session = Session()
-
-    first_state = session.query(State).order_by(State.id).first()
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
-    else:
+    try:
+        states = session.query(State.id, State.name).first()
+        print("{}: {}".format(states.id, states.name))
+    except Exception:
         print("Nothing")
     session.close()
