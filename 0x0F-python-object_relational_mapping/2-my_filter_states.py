7e3d2e263de66cb4b73894eb28aebe968c6667ad
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-08-07 23:02:24 -0500

    task2 Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument..

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
 
-"""lists all states from the database hbtn_0e_0_usa"""
-
+"""Takes an argument and displays all states"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    user = argv[1]
-    password = argv[2]
-    database = argv[3]
-    compare = argv[4]
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor()
 
-    db = MySQLdb.connect(host="localhost",
-                         user=user,
-                         passwd=password,
-                         db=database,
-                         port=3306)
-
-    cur = db.cursor()
-
-    cur.execute(
-        "SELECT * FROM states WHERE name\
-         LIKE '{}' ORDER BY states.id ASC".format(compare))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+        id ASC".format(argv[4]))
+    output = cursor.fetchall()
+    for item in output:
+        print(item)
+    cursor.close()
     db.close()
