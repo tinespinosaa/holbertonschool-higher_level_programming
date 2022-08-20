81f63b1a063fda7ff2cd967b863af37c814be68f
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-08-19 23:40:05 -0500

    task4 Write a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response..

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes a url as an arg and send a GET request 
-curl -sX GET -H "X-School-User-Id: 98" $1
+# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -s $1 -X GET -H "X-HolbertonSchool-User-Id:98"
