70eaeae33267e0c3291053080cce18706a69ec77
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-08-30 19:13:35 -0500

    task14 Write a Python file similar to model_state.py named model_city.py that contains the class definition of a City

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""
-This script prints all City objects
-from the database `hbtn_0e_14_usa`.
-"""
-
-from sys import argv
-from model_state import Base, State
+""" Prints all City objects """
 from model_city import City
-from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sys import argv
+
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
-
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
-    Session = sessionmaker(bind=engine)
-
+    sql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(sql.format(argv[1],
+                                      argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
     session = Session()
-
-    query = session.query(City, State).join(State)
-
-    for _c, _s in query.all():
-        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
-
-    session.commit()
+    for st, ct in session.query(State, City).\
+            order_by(City.id).\
+            filter(City.state_id == State.id).\
+            all():
+        print("{}: ({}) {}".format(st.name, ct.id, ct.name))
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""
-Python file to create class definition of a State and
-an instance Base = declarative_base()
-"""
-""" Importing modules """
-from model_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-""" Creating class """
+""" Contains the class definition of a City """
+from sqlalchemy import Column, Integer, String
+from model_state import Base
+from sqlalchemy import ForeignKey
 
 
 class City(Base):
-    """ Class City """
+    """Class city"""
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    state = relationship("State")
-
-    def __str__(self):
-        """__str__ attribute"""
-        return "{}".format(self.name)
